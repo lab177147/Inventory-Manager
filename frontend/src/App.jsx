c93fc38df08365a0d1aef3b3f0957b22559d28d7
Jakyubi <frostwth.xd@gmail.com>
2026-01-18 20:05:53 +0100

    feat: implement EditProduct component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import AddProduct from './AddProduct';
+import EditProduct from './EditProduct';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/add" element={<AddProduct />} />
-      <Route path="/edit/:id" element={<AddProduct />} /> 
+      <Route path="/edit/:id" element={<EditProduct />} /> 
     </Routes>
   );
 }
